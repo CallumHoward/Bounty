3d61904bba89dd07b68f7e6e7f3ab8b2d526617d
Chris <phibzy@gmail.com>
2015-06-01 23:32:37 +1000

    gogogogo

--- Agent.py
+++ Agent.py
@@ -22,6 +22,110 @@ def main():
         agent.makeBestMove()
         #agent.userControl()
 
+def makeBestMove(self):
+    self.smartBot()
+    #print 'FACING: |' + self.state.board.getLocation(self._getFacing()) + '|', '\t', self._getFacing()
+    #time.sleep(0.05) #TODO remove before submitting
+    #print '..'
+    #raw_input()
+
+
+def dumbBot(self):
+    if self.canRemoveTree():
+        self.removeTree()
+    elif self.canMoveForward() and random.randint(-1,4):
+        agent.moveForward()
+    else:
+        a = random.randint(-1,1)
+        if a == 1:
+            agent.turnLeft()
+        else:
+            self.turnRight()
+
+
+def smartBot(self):
+    if agent.getHasGold():
+        # follow shortest path back to starting point
+        path = agent.state.board.getShortestPath(agent.location, agent.origin, agent.getHasAxe(), agent.getNumDynamite())
+        agent.followPath(path)
+
+    # if the location of gold is known
+    if agent.state.getGoldLocation():
+        # if path to the location can be found, follow path
+        destination = agent.state.getGoldLocation()
+        path = agent.state.board.getShortestPath(agent.location, destination, agent.getHasAxe(), agent.getNumDynamite())
+        agent.followPath(path)
+
+    # if the location of axe is known
+    if agent.state.getAxeLocation():
+        agent.agentLog('FOUND AXE')
+        raw_input()
+        # if path to the location can be found, follow path
+        destination = agent.state.getAxeLocation()
+        path = agent.state.board.getShortestPath(agent.location, destination, agent.getHasAxe(), agent.getNumDynamite())
+        agent.followPath(path)
+
+    # collect any dynamite that can be seen
+    if agent.state.getDynamiteLocations():
+        agent.agentLog('COLLECTING EVERY DYNAMITE IN SIGHT')
+        for dynamite in agent.state.getDynamiteLocations():
+            raw_input()
+            # if path to the location can be found, follow path
+            path = agent.state.board.getShortestPath(agent.location, dynamite, agent.getHasAxe(), agent.getNumDynamite())
+            agent.followPath(path)
+
+    # chop down all the trees
+    if agent.getHasAxe() and agent.state.getTreeLocations():
+        agent.agentLog('CHOPPING EVERY TREE IN SIGHT')
+        for tree in agent.state.getTreeLocations():
+            raw_input()
+            # if path to the location can be found, follow path
+            path = agent.state.board.getShortestPath(agent.location, tree, agent.getHasAxe(), agent.getNumDynamite())
+            agent.agentLog(path)
+            agent.followPath(path)
+            agent.chopTree()
+
+    dumbBot()
+
+def exploreBot():
+    # bfs to nearest unexplored location
+    destination = agent.state.board.getNearestUnexplored()
+    path = agent.state.board.getShortestPath(agent.location, destination, agent.getHasAxe(), agent.getNumDynamite())
+    agent.followPath(path)
+
+
+def userControl():
+    print 'FACING: |' + agent.state.board.getLocation(agent._getFacing()) + '|', '\t', agent._getFacing()
+    print 'Move: ',
+    input = raw_input()
+
+    if input == 'f' and agent.canMoveForward():
+        agent.moveForward()
+    elif input == 'l':
+        agent.turnLeft()
+    elif input == 'r':
+        agent.turnRight()
+    elif input == 's':  #DEBUG
+        destination = agent.state.board.getUp(agent.location)
+        destination = agent.state.board.getUp(destination)
+        destination = self.state.board.getLeft(destination)
+        self.state.board.getShortestPath(self.location, destination, self.getHasAxe(), self.getNumDynamite())
+    elif input == 'b':
+        self.removeWall()
+    elif input == 'c':
+        self.removeTree()
+    elif input == 'g':
+        self.moveLeft()
+    elif input == 'y':
+        self.moveUp()
+    elif input == 'h':
+        self.moveDown()
+    elif input == 'j':
+        self.moveRight()
+    else:
+        print 'can\'t move!'
+        exit()
+
 # call main function only if not imported as a module
 if __name__ == '__main__':
     main()
